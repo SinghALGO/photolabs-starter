3ca506c01e7cd067629cbca9b5358e952546924c
SinghALGO <karanssandhu92@gmail.com>
2024-01-11 23:21:26 -0800

    Add:  photoListItem component added

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -2,20 +2,18 @@ import React from "react";
 
 import "../styles/PhotoListItem.scss";
 
-
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
-
-const PhotoListItem = () => {
-  /* Insert React */
+const PhotoListItem = ({ sampleDataForPhotoListItem }) => {
+  return (
+    <section className="photoList">
+      <img src={sampleDataForPhotoListItem.imageSource}></img>
+      <img src={sampleDataForPhotoListItem.profile}></img>
+      <h3>{sampleDataForPhotoListItem.username}</h3>
+      <h3>
+        {sampleDataForPhotoListItem.location.city},
+        {sampleDataForPhotoListItem.location.country}
+      </h3>
+    </section>
+  );
 };
 
 export default PhotoListItem;
