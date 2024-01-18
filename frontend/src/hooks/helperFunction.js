0650df350d7eff49b500196b239283bd15276f3b
SinghALGO <karanssandhu92@gmail.com>
2024-01-18 10:31:57 -0800

    Reducer added in different file

---
+++ frontend/src/hooks/helperFunction.js
@@ -0,0 +1,34 @@
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "SET_MODAL_DATA":
+      return {
+        ...state,
+        photoData: action.payload,
+        modalStatus: !state.modalStatus,
+      };
+    case "ADD_PHOTO_DATA":
+      return {
+        ...state,
+        photos: action.payload,
+      };
+    case "ADD_TOPIC_DATA":
+      return {
+        ...state,
+        topics: action.payload,
+      };
+    case "SET_TOPIC_ID":
+      return {
+        ...state,
+        topicId: action.payload,
+      };
+    case "TOGGLE_LIKE_PHOTO":
+      const isPhotoAlreadyLiked = state.likePhotoArray.includes(action.payload);
+      const newLikedArray = isPhotoAlreadyLiked
+        ? state.likePhotoArray.filter((photo) => photo !== action.payload)
+        : [...state.likePhotoArray, action.payload];
+      return { ...state, likePhotoArray: newLikedArray };
+    default:
+      throw new Error(`Unhandled action type: ${action.type}`);
+  }
+};
+export default reducer;
--- frontend/src/hooks/useApplicationData.js
+++ frontend/src/hooks/useApplicationData.js
@@ -1,5 +1,6 @@
 import { useReducer, useEffect } from "react";
 import axios from "axios";
+import reducer from "./helperFunction";
 const useApplicationData = () => {
   const initialState = {
     modalStatus: false,
@@ -9,41 +10,7 @@ const useApplicationData = () => {
     topics: [],
     topicId: "",
   };
-  const reducer = (state, action) => {
-    switch (action.type) {
-      case "SET_MODAL_DATA":
-        return {
-          ...state,
-          photoData: action.payload,
-          modalStatus: !state.modalStatus,
-        };
-      case "ADD_PHOTO_DATA":
-        return {
-          ...state,
-          photos: action.payload,
-        };
-      case "ADD_TOPIC_DATA":
-        return {
-          ...state,
-          topics: action.payload,
-        };
-      case "SET_TOPIC_ID":
-        return {
-          ...state,
-          topicId: action.payload,
-        };
-      case "TOGGLE_LIKE_PHOTO":
-        const isPhotoAlreadyLiked = state.likePhotoArray.includes(
-          action.payload
-        );
-        const newLikedArray = isPhotoAlreadyLiked
-          ? state.likePhotoArray.filter((photo) => photo !== action.payload)
-          : [...state.likePhotoArray, action.payload];
-        return { ...state, likePhotoArray: newLikedArray };
-      default:
-        throw new Error(`Unhandled action type: ${action.type}`);
-    }
-  };
+
   const [state, dispatch] = useReducer(reducer, initialState);
 
   useEffect(() => {
